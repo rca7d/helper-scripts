76330cbf8c1d7911147b8ac4bd29f86097287dc3
rca7d <rca7d@users.noreply.github.com>
2023-01-17 10:38:26 -0500

    Update vnet-ddos-query.ps1

--- powershell/vnet-ddos-query.ps1
+++ powershell/vnet-ddos-query.ps1
@@ -12,34 +12,51 @@ $subscriptions = Get-AzSubscription
 
 # Iterate through each subscription
 foreach ($subscription in $subscriptions) {
-  # Set the current subscription context
-  Select-AzSubscription -SubscriptionId $subscription.Id
+    # Set the current subscription context
+    Select-AzSubscription -SubscriptionId $subscription.Id
 
-  # Get all virtual networks in the subscription
-  $vnets = Get-AzVirtualNetwork
-  
-  # Iterate through each virtual network
-  foreach ($vnet in $vnets) {
-    # Check if the virtual network has a DDoS protection plan
-    if ($vnet.DdosProtectionPlan -ne $null) {
-      # Retrieve the DDoS protection plan for the virtual network
-      $ddos = Get-AzDdosProtectionPlan -ResourceGroupName $vnet.ResourceGroupName -Name $vnet.DdosProtectionPlan.Id
-    } else {
-      # If the virtual network does not have a DDoS protection plan, assign $null to the variable
-      $ddos = $null
+    # Get all virtual networks in the subscription
+    $vnets = Get-AzVirtualNetwork
+
+    # Iterate through each virtual network
+    foreach ($vnet in $vnets) {
+        # Check if the virtual network has a DDoS protection plan
+        if ($vnet.DdosProtectionPlan -ne $null) {
+            # Retrieve the DDoS protection plan for the virtual network
+            $ddos = Get-AzDdosProtectionPlan -Name $vnet.DdosProtectionPlan.Id -ErrorAction SilentlyContinue
+            if($ddos -eq $null)
+            {
+                # Iterate through all subscriptions to find the correct DDoS protection plan
+                foreach ($sub in $subscriptions) {
+                    if ($sub.Id -ne $subscription.Id) {
+                        # change subscription context
+                        Select-AzSubscription -SubscriptionId $sub.Id
+                        # check if the ddos plan exist in this subscription
+                        $ddos = Get-AzDdosProtectionPlan -Name $vnet.DdosProtectionPlan.Id -ErrorAction SilentlyContinue
+                        # if found, break the loop
+                        if($ddos -ne $null)
+                        {
+                            break
+                        }
+                    }
+                }
+            }
+        } else {
+          # If the virtual network does not have a DDoS protection plan, assign $null to the variable
+          $ddos = $null
+        }
+        # Retrieve the resource tags for the virtual network
+        $tags = (Get-AzResource -ResourceId $vnet.Id).Tags
+        # Creating an output object with the required properties
+        $output = [PSCustomObject]@{
+            SubscriptionName = $subscription.Name
+            VirtualNetworkName = $vnet.Name
+            DdosProtectionPlan = if($ddos -ne $null){$ddos.Name} else {"NA"}
+            Tags = $tags
+        }
+        # Output the object
+        Write-Output $output
     }
-    # Retrieve the resource tags for the virtual network
-    $tags = (Get-AzResource -ResourceId $vnet.Id).Tags
-    # Creating an output object with the required properties
-    $output = [PSCustomObject]@{
-      SubscriptionName = $subscription.Name
-      VirtualNetworkName = $vnet.Name
-      DdosProtectionPlan = if($ddos -ne $null){$ddos.Name} else {"NA"}
-      Tags = $tags
-    }
-    # Output the object
-    Write-Output $output
-  }
 }
 #Exporting the output to CSV file
 $output | Export-Csv -Path "VirtualNetworks.csv" -NoTypeInformation
