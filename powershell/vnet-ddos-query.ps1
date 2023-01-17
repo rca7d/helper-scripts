84ba8e29351c95937036a238876bc317d8d3194f
rca7d <rca7d@users.noreply.github.com>
2023-01-17 17:34:12 -0500

    Update vnet-ddos-query.ps1

--- powershell/vnet-ddos-query.ps1
+++ powershell/vnet-ddos-query.ps1
@@ -7,57 +7,61 @@ This script is intended to gather information about all virtual networks in all 
 # Connect to Azure account
 Connect-AzAccount
 
-# Login to the selected tenant
-$selectedTenantId = "<insert-tenant-id>"
-Write-Output "Using tenant $($selectedTenantId)"
+# Retrieve all subscriptions
+$subscriptions = Get-AzSubscription
 
 # Create an empty hash table to store the DDoS protection plan and virtual network objects
 $cache = @{}
 
-$subscriptions = Get-AzSubscription | Where-Object {$_.TenantId -eq $selectedTenantId -and $_.State -eq 'Enabled'}
-
-# Retrieve all DDoS protection plans and virtual networks across all subscriptions
-Write-Output "Retrieving DDoS protection plans and virtual networks for all subscriptions"
-$ddosPlans = Get-AzDdosProtectionPlan -All
-$vnets = Get-AzVirtualNetwork -All
-# Add the DDoS protection plans and virtual networks to the cache
-$cache.Add("ddos", $ddosPlans)
-$cache.Add("vnets", $vnets)
-
+# Iterate through each subscription
 foreach ($subscription in $subscriptions) {
-    
-    Write-Output "Processing subscription $($subscription.Name) ($($subscription.Id))"
-    $ddosPlans = $cache["ddos"]
-    $vnets = $cache["vnets"]
+    Select-AzSubscription -SubscriptionId $subscription.Id
+    Write-Output "Retrieving DDoS protection plans and virtual networks for subscription $($subscription.Name) ($($subscription.Id))"
+    # Retrieve DDoS protection plans and virtual networks for the current subscription
+    $ddosPlans = Get-AzDdosProtectionPlan -SubscriptionId $subscription.Id
+    $vnets = Get-AzVirtualNetwork -SubscriptionId $subscription.Id
+    # Add the DDoS protection plans and virtual networks to the cache
+    $cache.Add($subscription.Id, @{
+        ddosPlans = $ddosPlans
+        vnets = $vnets
+    })
+}
 
-    # Iterate through each virtual network
-    foreach ($vnet in $vnets) {
-        Write-Output "Processing virtual network $($vnet.Name)"
-        # Get the DDoS protection plan object
-        if ($vnet.DdosProtectionPlan -ne $null) {
-            $ddos = $ddosPlans | Where-Object { $_.Id -eq $vnet.DdosProtectionPlan.Id }
-            if ($ddos) {
-                $ddos = $ddos[0]
-            }
+$allVnets = $cache.Values | foreach {$_.vnets}
+
+Write-Output "Retrieved $(($allVnets | Measure-Object).Count) virtual networks across all subscriptions"
+
+# Create an empty array to store the output objects
+$outputs = @()
+
+# Iterate through each virtual network
+foreach ($vnet in $allVnets) {
+    Write-Output "Processing virtual network $($vnet.Name)"
+    # Get the DDoS protection plan object
+    if ($vnet.DdosProtectionPlan -ne $null) {
+        $ddosName = $cache[$vnet.SubscriptionId].ddosPlans | Where-Object {$_.Id -eq $vnet.DdosProtectionPlan.Id}
+        if ($ddos) {
+            $ddosName = $ddos.Name
         } else {
-            $ddos = $null
+            $ddosName = "NA"
         }
-        # Get the resource group object
-        $rg = Get-AzResourceGroup -Name $vnet.ResourceGroupName
-        # Get the tags for the virtual network
-        $tags = (Get-AzResource -ResourceId $vnet.Id).Tags
-        $tags = ($tags | ConvertTo-Json)
-        # Create an object to store the output for the current virtual network
-        $output = [PSCustomObject]@{
-            SubscriptionName = $subscription.Name
-            VirtualNetworkName = $vnet.Name
-            DDOSProtectionPlan = $ddos.Name
-            ResourceGroup = $vnet.ResourceGroupName
-            Tags = $tags
-        }
-        # Add the output object to the array of output objects
-        $outputs += $output
+    } else {
+        $ddosName = "NA"
     }
+    # Get the resource group name from the vnet object
+    $rgName = $vnet.ResourceGroupName
+    # Get the tags for the virtual network
+    $tags = (Get-AzResource -ResourceId $vnet.Id).Tags
+    # Create an object to store the output for the current virtual network
+    $output = [PSCustomObject]@{
+        SubscriptionName = $subscription.Name
+        VirtualNetworkName = $vnet.Name
+        DDOSProtectionPlanName = $ddos
+        ResourceGroupName = $rgName
+        Tags = $tags
+    }
+    # Add the output object to the array of output objects
+    $outputs += $output
 }
 
 # Export the output objects to a CSV file
